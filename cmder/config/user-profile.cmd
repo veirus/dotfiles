4ca47e08866b5489333c1935e64d92857e4e4b57
veirus <thetauleherves@gmail.com>
2017-06-23 20:58:12 +0500

    + cmder configs

---
+++ cmder/config/user-profile.cmd
@@ -0,0 +1,8 @@
+:: use this file to run your own startup commands
+:: use  in front of the command to prevent printing the command
+
+:: call "%GIT_INSTALL_ROOT%/cmd/start-ssh-agent.cmd"
+:: set "PATH=%CMDER_ROOT%\vendor\whatever;%PATH%"
+
+:: @prompt $E[1;30;40m[$T]$S$E[1;32;40m$P$S$_$E[1;30;40m$G$G$G$S$E[0m
+rem @prompt $E[1;30;40m[$T]$S$E[1;32;40m$P$S$_$E[1;30;40m{lamb}$G$G$G$S$E[0m
